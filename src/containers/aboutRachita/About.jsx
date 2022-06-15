06a92097af98629ed86f0f6ddbe6a46701f73f59
Akshith <akshithgandla7891@gmail.com>
2022-06-16 00:27:55 +0530

    Add the quote in About Section

--- src/containers/aboutRachita/About.jsx
+++ src/containers/aboutRachita/About.jsx
@@ -1,30 +1,43 @@
 import React from "react";
 import "./about.css";
 import aboutRachitaImage from "../../images/aboutRachitaImg.png";
+import quoteContainer from "../../images/quoteContainer.png";
+import quote from "../../images/quote.png";
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="about__header">
-        <div className="about__image">
-          <img src={aboutRachitaImage} alt="" />
+    <div>
+      <div className="about">
+        <div className="about__header">
+          <div className="about__image">
+            <img src={aboutRachitaImage} alt="" />
+          </div>
+          <div className="about_content">
+            <h2>About Rachita</h2>
+            <p>
+              Rachita Sharma is a technology entrepreneur, financial literacy
+              advocate and gender rights activist. Rachita is the CEO of Girl
+              Power Talk, based in India, a purpose driven organization
+              empowering today’s most capable youth with the confidence,
+              knowledge and opportunity to become tomorrow’s global leaders. Her
+              responsibilities include corporate communications, client
+              engagement, and new business development. She leads the evaluation
+              of all new potential Girl Power Talk team members and expansion
+              into new markets.
+            </p>
+          </div>
         </div>
-        <div className="about_content">
-          <h2>About Rachita</h2>
-          <p>
-            Rachita Sharma is a technology entrepreneur, financial literacy
-            advocate and gender rights activist. Rachita is the CEO of Girl
-            Power Talk, based in India, a purpose driven organization empowering
-            today’s most capable youth with the confidence, knowledge and
-            opportunity to become tomorrow’s global leaders. Her
-            responsibilities include corporate communications, client
-            engagement, and new business development. She leads the evaluation
-            of all new potential Girl Power Talk team members and expansion into
-            new markets.
-          </p>
-        </div>
+        <div className="about__quote"></div>
       </div>
-      <div className="about__quote"></div>
+      <div className="quote">
+        <img src={quoteContainer} alt="" />
+        <img src={quote} className="quote__image_1"></img>'
+        <img src={quote} className="quote__image_2"></img>
+        <p>
+          One girl empowers another. Let’s change lives together: one girl, one
+          woman and one human being at a time.
+        </p>
+      </div>
     </div>
   );
 };

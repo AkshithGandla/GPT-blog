c7b090e400f084493e37de02fea471268d32e191
Akshith <akshithgandla7891@gmail.com>
2022-07-10 01:43:06 +0530

    make the News section outline

--- src/containers/News/News.jsx
+++ src/containers/News/News.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./news.css";
+import newsData from "./NewsData.json";
 import news1 from "../../images/news1.png";
 import news2 from "../../images/news2.png";
 import news3 from "../../images/news3.png";
@@ -7,12 +8,35 @@ const News = () => {
   return (
     <div>
       <div className="newsDiv">
-        <div>
-          <img src={news1}></img>
+        <div className="newsMain">
+          <div className="newsMainImg">
+            <img src={newsData[0].NewsCardimg}></img>
+          </div>
+          <div className="newsMainContent">
+            <h2>{newsData[0].NewsCardHeading}</h2>
+            <p>{newsData[0].NewsCardText}</p>
+          </div>
         </div>
+
         <div className="subNewsDiv">
-          <img src={news2}></img>
-          <img src={news3}></img>
+          <div className="subnews_1">
+            <div className="subnews_1__img">
+              <img src={newsData[1].NewsCardimg}></img>
+            </div>
+            <div className="subnews_1__content">
+              <h2>{newsData[1].NewsCardHeading}</h2>
+              <p>{newsData[1].NewsCardText}</p>
+            </div>
+          </div>
+          <div className="subnews_1">
+            <div className="subnews_1__img">
+              <img src={newsData[2].NewsCardimg}></img>
+            </div>
+            <div className="subnews_1__content">
+              <h2>{newsData[2].NewsCardHeading}</h2>
+              <p>{newsData[2].NewsCardText}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
